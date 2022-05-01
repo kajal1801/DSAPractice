b9977ef86e36859d670c07dce7866fbd8f936664
kajal1801 <anshikajais2001@gmail.com>
2022-05-01 10:56:04 +0530

    Minimum and maximum in a BT

---
+++ Trees/MinMaxTree.java
@@ -0,0 +1,39 @@
+class Node{
+    int data;
+    Node right, left;
+    Node(int k){
+        data = k;
+    }
+}
+
+class MinMaxTree{
+    public static int findMax(Node root){
+        //code here
+        if(root ==  null){
+            return Integer.MIN_VALUE;
+        }
+        return Math.max(Math.max(findMax(root.left),findMax(root.right)),root.data);
+    }
+    public static int findMin(Node root){
+        //code here
+        if(root == null){
+            return Integer.MAX_VALUE;
+        }
+        return Math.min(Math.min(findMin(root.left),findMin(root.right)),root.data);
+    }
+
+    public static void main(String args[]){
+        Node root = new Node(12);
+        root.left = new Node(45);
+        root.right = new Node(69);
+        root.left.left = new Node(56);
+        root.left.right = new Node(89);
+        root.right.right = new Node(90);
+
+        int ans;
+        ans = findMax(root);
+        System.out.println("Maximum element in the given tree is : "+ans);
+        ans = findMin(root);
+        System.out.println("Minimum element in the given tree is : "+ans);
+    }
+}
