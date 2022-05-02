9c1e018c3c0314e9e93b9b0cef0ae13665c245f7
kajal1801 <anshikajais2001@gmail.com>
2022-05-02 12:29:42 +0530

    Height of the tree

--- Trees/TreeHeight.java
+++ Trees/TreeHeight.java
@@ -1,15 +1,15 @@
-class Node{
+class Node5{
     int key;
-    Node right;
-    Node left;
-    Node(int k){
+    Node5 right;
+    Node5 left;
+    Node5(int k){
         key = k;
     }
 }
 
 public class TreeHeight {
 
-    public int height(Node root){
+    public int height(Node5 root){
         if(root == null){
             return 0;
         }
@@ -19,12 +19,12 @@ public class TreeHeight {
     }
 
     public static void main(String args[]){
-        Node root = new Node(10);
-        root.left = new Node(8);
-        root.right = new Node(30);
-        root.right.left = new Node(40);
-        root.right.right = new Node(50);
-        root.right.right.left = new Node(56);
+        Node5 root = new Node5(10);
+        root.left = new Node5(8);
+        root.right = new Node5(30);
+        root.right.left = new Node5(40);
+        root.right.right = new Node5(50);
+        root.right.right.left = new Node5(56);
 
         TreeHeight obj = new TreeHeight();
         int h = obj.height(root);
