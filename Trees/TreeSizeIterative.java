b768df688a5b437b6955c0b69761ef8d149c607e
kajal1801 <anshikajais2001@gmail.com>
2022-05-02 12:30:44 +0530

    Size of the Tree using Loop

--- Trees/TreeSizeIterative.java
+++ Trees/TreeSizeIterative.java
@@ -1,26 +1,26 @@
 import java.util.LinkedList;
 import java.util.Queue;
 
-class Node{
+class Node3{
     int key;
-    Node right, left;
-    Node(int k){
+    Node3 right, left;
+    Node3(int k){
         key = k;
     }
 }
 
 public class TreeSizeIterative {
     
-    public int calculateSize(Node root){
+    public int calculateSize(Node3 root){
         if(root == null){
             return 0;
         }
-        Queue<Node> q = new LinkedList<Node>();
+        Queue<Node3> q = new LinkedList<Node3>();
         int count = 0;
         q.add(root);
         while(q.isEmpty() == false){
             count++;
-            Node curr = q.poll();
+            Node3 curr = q.poll();
             if(curr.left != null){
                 q.add(curr.left);
             }
@@ -33,12 +33,12 @@ public class TreeSizeIterative {
 
     public static void main(String args[]){
         TreeSizeIterative obj = new TreeSizeIterative();
-        Node root = new Node(12);
-        root.left = new Node(45);
-        root.right = new Node(69);
-        root.left.left = new Node(56);
-        root.left.right = new Node(89);
-        root.right.right = new Node(90);
+        Node3 root = new Node3(12);
+        root.left = new Node3(45);
+        root.right = new Node3(69);
+        root.left.left = new Node3(56);
+        root.left.right = new Node3(89);
+        root.right.right = new Node3(90);
 
         int size = obj.calculateSize(root);
         System.out.println("Size of the given tree is : "+size);
