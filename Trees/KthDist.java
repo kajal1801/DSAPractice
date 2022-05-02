73f32aea1644976d9b2532a10a3d48c44ae26298
kajal1801 <anshikajais2001@gmail.com>
2022-05-02 12:27:11 +0530

    Finding node at kth distance from root

--- Trees/KthDist.java
+++ Trees/KthDist.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
 
-class Node{
+class Node14{
     int key;
-    Node left;
-    Node right;
-    Node(int k){
+    Node14 left;
+    Node14 right;
+    Node14(int k){
         key = k;
     }
 }
 
 public class KthDist {
 
-    public void printkDist(Node root, int k){
+    public void printkDist(Node14 root, int k){
         if(root == null){
             return;
         }
@@ -27,14 +27,14 @@ public class KthDist {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         KthDist obj = new KthDist();
-        Node root = new Node(10);
-        root.left = new Node(20);
-        root.right = new Node(30);
-        root.left.left = new Node(40);
-        root.left.right = new Node(50);
-        root.right.right = new Node(70);
+        Node14 root = new Node14(10);
+        root.left = new Node14(20);
+        root.right = new Node14(30);
+        root.left.left = new Node14(40);
+        root.left.right = new Node14(50);
+        root.right.right = new Node14(70);
         
-        System.out.println("Enter the distance whose Node you want to print...Keep it within 2");
+        System.out.println("Enter the distance whose Node14 you want to print...Keep it within 2");
         int k = sc.nextInt();
 
         obj.printkDist(root, k);
