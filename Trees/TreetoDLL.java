6f1303f239b4faa8b2c37db8fda1573acfd594c0
kajal1801 <anshikajais2001@gmail.com>
2022-05-02 22:32:16 +0530

    Converting Binary Tree to Doubly Linked List

---
+++ Trees/TreetoDLL.java
@@ -0,0 +1,134 @@
+import java.util.*;
+import java.io.*;
+
+class Node
+{
+	Node left, right;
+	int data;
+	
+	Node(int d)
+	{
+		data = d;
+		left = right = null;
+	}
+	
+}
+class TreetoDLL
+{
+    
+    static Node buildTree(String str)throws IOException{
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        Node root = new Node(Integer.parseInt(ip[0]));
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            String currVal = ip[i];
+                
+            if(!currVal.equals("N")) {
+                    
+                currNode.left = new Node(Integer.parseInt(currVal));
+                queue.add(currNode.left);
+            }
+                
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    
+
+	void inorder(Node node)
+	{
+		if(node==null)
+			return ;
+		else
+			inorder(node.left);
+			System.out.print(node.data + " ");
+			inorder(node.right);
+	}
+	public static void printList(Node head) 
+    {
+		Node prev = head;
+        while (head != null) 
+        {
+            System.out.print(head.data + " ");
+			prev = head;
+            head = head.right;
+        }
+		
+		System.out.println();
+		while(prev != null)
+		{
+			System.out.print(prev.data+" ");
+			prev = prev.left;
+		}
+    }
+	public static void main(String args[])  throws IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t > 0){
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+        	    Ttodll g = new Ttodll();
+			    
+			    Node ans = g.bToDLL(root);
+			    printList(ans);
+                t--;
+                System.out.println();
+	        }
+			
+		
+	}
+}
+
+class Ttodll
+{
+    Node prev = null;
+    Node bToDLL(Node root)
+    {
+	    //  Your code here	
+	    if(root == null){
+	        return root;
+	    }
+	    Node head = bToDLL(root.left);
+	    if(prev == null){
+	        head = root;
+	    }
+	    else{
+	        root.left = prev;
+	        prev.right = root;
+	    }
+	    prev = root;
+	    bToDLL(root.right);
+	    return head;
+    }
+}
