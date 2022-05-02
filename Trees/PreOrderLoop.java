ff1865e4ba61e9cf3860493cefc8a883ed01b627
kajal1801 <anshikajais2001@gmail.com>
2022-05-02 12:29:06 +0530

    Preorder Traversal using Loop

--- Trees/PreOrderLoop.java
+++ Trees/PreOrderLoop.java
@@ -2,10 +2,10 @@
 import java.util.ArrayList;
 import java.util.Stack;
 
-class Node{
+class Node7{
     int data;
-    Node left,right;
-    Node(int d){
+    Node7 left,right;
+    Node7(int d){
         data=d;
         left=right=null;
     }
@@ -14,15 +14,15 @@ class Node{
 public class PreOrderLoop
 {
     //Function to return a list containing the preorder traversal of the tree.
-    static ArrayList<Integer> preorder(Node root)
+    static ArrayList<Integer> preorder(Node7 root)
     {
         ArrayList<Integer> list = new ArrayList<Integer>();
         if(root == null){
             return list;
         }
         // Code here
-        Node curr = root;
-        Stack<Node> st = new Stack<Node>();
+        Node7 curr = root;
+        Stack<Node7> st = new Stack<Node7>();
         while(curr != null || !st.isEmpty()){
             while(curr != null){
                 list.add(curr.data);
