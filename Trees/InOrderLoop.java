6eb618496d9eade5c308022ec07b1a8a9e75ba05
kajal1801 <anshikajais2001@gmail.com>
2022-05-02 12:26:23 +0530

    InOrder Traversal using loop

--- Trees/InOrderLoop.java
+++ Trees/InOrderLoop.java
@@ -2,24 +2,24 @@
 import java.util.ArrayList;
 import java.util.Stack;
 
-class Node {
+class Node16 {
     int data;
-    Node left, right;
-   Node(int item)    {
+    Node16 left, right;
+   Node16(int item)    {
         data = item;
         left = right = null;
     }
 }
 public class InOrderLoop {
     // Function to return a list containing the inorder traversal of the tree.
-    ArrayList<Integer> inOrder(Node root) {
+    ArrayList<Integer> inOrder(Node16 root) {
         // Code
         ArrayList<Integer> list = new ArrayList<Integer>();
         if(root == null){
             return list;
         }
-        Stack<Node> st = new Stack<Node>();
-        Node curr = root;
+        Stack<Node16> st = new Stack<Node16>();
+        Node16 curr = root;
         while(curr != null || !st.isEmpty()){
             if(curr != null){
                 st.push(curr);
