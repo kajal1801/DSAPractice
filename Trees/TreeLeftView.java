d57dc52fb7086f70d79bb2e05e8a780d1a6dc22b
kajal1801 <anshikajais2001@gmail.com>
2022-05-02 12:30:00 +0530

    Left View of the Tree

--- Trees/TreeLeftView.java
+++ Trees/TreeLeftView.java
@@ -1,14 +1,14 @@
-class Node{
+class Node4{
     int key;
-    Node right, left;
-    Node(int k){
+    Node4 right, left;
+    Node4(int k){
         key = k;
     }
 }
 
 public class TreeLeftView {
     
-    public int calculateSize(Node root){
+    public int calculateSize(Node4 root){
         if(root == null){
             return 0;
         }
@@ -17,12 +17,12 @@ public class TreeLeftView {
 
     public static void main(String args[]){
         TreeLeftView obj = new TreeLeftView();
-        Node root = new Node(12);
-        root.left = new Node(45);
-        root.right = new Node(69);
-        root.left.left = new Node(56);
-        root.left.right = new Node(89);
-        root.right.right = new Node(90);
+        Node4 root = new Node4(12);
+        root.left = new Node4(45);
+        root.right = new Node4(69);
+        root.left.left = new Node4(56);
+        root.left.right = new Node4(89);
+        root.right.right = new Node4(90);
 
         int size = obj.calculateSize(root);
         System.out.println("Size of the given tree is : "+size);
