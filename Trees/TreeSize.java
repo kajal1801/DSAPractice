dc1f0b2ddd872dce18550bc9b7d975782119f465
kajal1801 <anshikajais2001@gmail.com>
2022-05-02 12:30:17 +0530

    Size of the tree

--- Trees/TreeSize.java
+++ Trees/TreeSize.java
@@ -1,14 +1,14 @@
-class Node{
+class Node2{
     int key;
-    Node right, left;
-    Node(int k){
+    Node2 right, left;
+    Node2(int k){
         key = k;
     }
 }
 
 public class TreeSize {
     
-    public int calculateSize(Node root){
+    public int calculateSize(Node2 root){
         if(root == null){
             return 0;
         }
@@ -17,12 +17,12 @@ public class TreeSize {
 
     public static void main(String args[]){
         TreeSize obj = new TreeSize();
-        Node root = new Node(12);
-        root.left = new Node(45);
-        root.right = new Node(69);
-        root.left.left = new Node(56);
-        root.left.right = new Node(89);
-        root.right.right = new Node(90);
+        Node2 root = new Node2(12);
+        root.left = new Node2(45);
+        root.right = new Node2(69);
+        root.left.left = new Node2(56);
+        root.left.right = new Node2(89);
+        root.right.right = new Node2(90);
 
         int size = obj.calculateSize(root);
         System.out.println("Size of the given tree is : "+size);
