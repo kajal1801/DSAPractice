b79679f8f2a61c72edf7b4e9d52ef0d3c23c3bc0
kajal1801 <anshikajais2001@gmail.com>
2022-05-02 12:28:51 +0530

    Post order Traversal using recursion

--- Trees/PostorderTraversal.java
+++ Trees/PostorderTraversal.java
@@ -1,15 +1,15 @@
-class Node{
+class Node8{
     int key;
-    Node left;
-    Node right;
-    Node(int k){
+    Node8 left;
+    Node8 right;
+    Node8(int k){
         key = k;
     }
 }
 
 public class PostorderTraversal {
 
-    public void postorder(Node root){
+    public void postorder(Node8 root){
         if(root != null){
             postorder(root.left);
             postorder(root.right);
@@ -18,11 +18,11 @@ public class PostorderTraversal {
     }
 
     public static void main(String args[]){
-        Node root = new Node(10);
-        root.left = new Node(20);
-        root.right = new Node(30);
-        root.right.left = new Node(40);
-        root.right.right = new Node(50);
+        Node8 root = new Node8(10);
+        root.left = new Node8(20);
+        root.right = new Node8(30);
+        root.right.left = new Node8(40);
+        root.right.right = new Node8(50);
 
         PostorderTraversal obj = new PostorderTraversal();
         obj.postorder(root);
