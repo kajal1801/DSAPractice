3791d28b6947a273aa211efa7679319d9ef88e68
kajal1801 <anshikajais2001@gmail.com>
2022-05-02 12:27:27 +0530

    Level Order Traversal Line by Line

--- Trees/LevelOrderLinebyLine.java
+++ Trees/LevelOrderLinebyLine.java
@@ -1,24 +1,24 @@
 import java.util.LinkedList;
 import java.util.Queue;
 
-class Node{
+class Node13{
     int key;
-    Node right;
-    Node left;
-    Node(int k){
+    Node13 right;
+    Node13 left;
+    Node13(int k){
         key = k;
     }
 }
 public class LevelOrderLinebyLine {
-    public void printLevel(Node root){
+    public void printLevel(Node13 root){
         if(root == null){
             return;
         }
-        Queue<Node> q = new LinkedList<Node>();
+        Queue<Node13> q = new LinkedList<Node13>();
         q.add(root);
         q.add(null);
         while(q.size() > 1){
-            Node curr = q.poll();
+            Node13 curr = q.poll();
             if(curr == null){
                 System.out.println();
                 q.add(null);
@@ -36,12 +36,12 @@ public class LevelOrderLinebyLine {
 
     public static void main(String args[]){
         LevelOrderLinebyLine obj = new LevelOrderLinebyLine();
-        Node root = new Node(12);
-        root.left = new Node(45);
-        root.right = new Node(69);
-        root.left.left = new Node(56);
-        root.left.right = new Node(89);
-        root.right.right = new Node(90);
+        Node13 root = new Node13(12);
+        root.left = new Node13(45);
+        root.right = new Node13(69);
+        root.left.left = new Node13(56);
+        root.left.right = new Node13(89);
+        root.right.right = new Node13(90);
 
         obj.printLevel(root);
     }
