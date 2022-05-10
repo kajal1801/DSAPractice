05dce3b5c08a80f4670a91a9a828d0f7b43490bb
kajal1801 <anshikajais2001@gmail.com>
2022-05-10 20:55:19 +0530

    Number of subsets with the given sum

---
+++ Recursion/SubsetSumK3.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class SubsetSumK3 {
+    public int subsetsum(int arr[], int i, int s, int sum){
+        if(i == arr.length){
+            if(s == sum){
+                return 1;
+            }
+            return 0;
+        }
+    
+        s+=arr[i];
+        int lt = subsetsum(arr, i+1, s, sum);
+        s-=arr[i];
+        int rt = subsetsum(arr, i+1, s, sum);
+        return lt+rt;
+    }
+
+    public static void main(String args[]){
+        SubsetSumK3 obj = new SubsetSumK3();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array : ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter the elements of the array: ");
+        for(int i = 0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Enter the sum you want check for: ");
+        int sum = sc.nextInt();
+        int ans = obj.subsetsum(arr, 0, 0, sum);
+        System.out.println("The number of Subsets with sum " + sum + " is " + ans);
+        sc.close();
+    }
+}
