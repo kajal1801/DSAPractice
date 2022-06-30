15050af3dbdeadf5cd11b2ffa0ceb6587e2d42ff
kajal1801 <anshikajais2001@gmail.com>
2022-07-01 03:08:05 +0530

    Search an element in a rotated array

--- Searching/SearchRotated.java
+++ Searching/SearchRotated.java
@@ -2,18 +2,26 @@ import java.util.Scanner;
 
 public class SearchRotated {
 
-    public int search(int arr[], int key, int lb, int ub){
+    public int search(int arr[], int lb, int ub, int key){
         if(lb > ub){
             return -1;
         }
-        int mid = lb + ((ub-lb)/2);
+        int mid = lb + ((ub - lb)/2);
         if(arr[mid] == key){
             return mid;
         }
-        else if(arr[mid] > arr[0] && arr[0] < key){
-            return search(arr, key, lb, mid-1);
+        if(arr[mid] > arr[lb]){
+            if(arr[mid] > key && arr[lb] <= key){
+                return search(arr, lb, mid-1, key);
+            }
+            else{
+                return search(arr, mid+1, ub, key);
+            }
         }
-        return search(arr, key, mid+1, ub);
+        if(arr[mid] < key && arr[ub] >= key){
+            return search(arr, mid+1,ub,key);
+        }
+        return search(arr, lb, mid-1,key);
     }
 
     public static void main(String args[]){
@@ -28,7 +36,7 @@ public class SearchRotated {
         }
         System.out.println("Enter the element to be searched: ");
         int k = sc.nextInt();
-        int res = obj.search(arr, k, 0, n-1);
+        int res = obj.search(arr, 0, n-1, k);
         if(res > -1){
             System.out.println("The given element was found at the index: " + res);
         }
