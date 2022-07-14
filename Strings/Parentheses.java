02613a97db790c2d2d3b26143fe4aa5df6fc774a
kajal1801 <anshikajais2001@gmail.com>
2022-07-14 14:56:44 +0530

    Valid Parentheses Leetcode

---
+++ Strings/Parentheses.java
@@ -0,0 +1,23 @@
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for(int i = 0;i<s.length();i++){
+            char c = s.charAt(i);
+            if(c == ')' && !st.isEmpty() && st.peek() == '('){
+                st.pop();
+            }
+            else if(c == '}' && !st.isEmpty() && st.peek() == '{'){
+                st.pop();
+            }
+            else if(c == ']' && !st.isEmpty() && st.peek() == '['){
+                st.pop();
+            }
+            else{
+                st.push(c);
+            }
+        }
+        return st.isEmpty();
+    }
+}
