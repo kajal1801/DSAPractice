0d7611ca6a09740602fc59b0bded40cbc80d45a5
kajal1801 <anshikajais2001@gmail.com>
2022-09-05 23:08:10 +0530

    ZigZag Tree Traversal - Leetcode

--- Trees/ZigZagTraversal.java
+++ Trees/ZigZagTraversal.java
@@ -1,3 +1,9 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class ZigZagTraversal {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         if(root == null){
