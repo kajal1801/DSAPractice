855468c454a1f1098f942a961b75dae3b887a78b
Kajal Jaiswal <anshikajais2001@gmail.com>
2022-09-08 11:52:34 +0530

    N-ary Tree Level Order - Leetcode

--- Trees/LevelOrderNary.java
+++ Trees/LevelOrderNary.java
@@ -3,37 +3,37 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
-class Node {
+class Node30 {
     public int val;
-    public List<Node> children;
+    public List<Node30> children;
 
-    public Node() {}
+    public Node30() {}
 
-    public Node(int _val) {
+    public Node30(int _val) {
         val = _val;
     }
 
-    public Node(int _val, List<Node> _children) {
+    public Node30(int _val, List<Node30> _children) {
         val = _val;
         children = _children;
     }
 }
 
 public class LevelOrderNary{
-    public List<List<Integer>> levelOrder(Node root) {
+    public List<List<Integer>> levelOrder(Node30 root) {
         List<List<Integer>> list = new ArrayList<List<Integer>>();
         if(root == null){
             return list;
         }
-        Queue<Node> q = new LinkedList<Node>();
+        Queue<Node30> q = new LinkedList<Node30>();
         q.add(root);
         while(!q.isEmpty()){
             List<Integer> curr = new ArrayList<Integer>();
             int size = q.size();
             for(int i = 0;i < size;i++){
-                Node temp = q.poll();
+                Node30 temp = q.poll();
                 curr.add(temp.val);
-                for(Node child : temp.children){
+                for(Node30 child : temp.children){
                     q.add(child);
                 }
             }
