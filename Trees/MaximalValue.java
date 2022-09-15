c0f84daedc84be5a53adaf5eaca523386c7d0b12
Kajal Jaiswal <anshikajais2001@gmail.com>
2022-09-15 22:08:45 +0530

    Count Maximal Value of Roots - BinarySearch

--- Trees/MaximalValue.java
+++ Trees/MaximalValue.java
@@ -1,8 +1,8 @@
-class Tree {
+class Tree1 {
   int val;
-  Tree left;
-  Tree right;
-  Tree(int data){
+  Tree1 left;
+  Tree1 right;
+  Tree1(int data){
     this.val = data;
     this.right = null;
     this.left = null;
@@ -11,7 +11,7 @@ class Tree {
  
 public class MaximalValue {
     int count = 0;
-    public int sum(Tree root, int max){
+    public int sum(Tree1 root, int max){
         if(root == null){
             return 0;
         }
@@ -30,7 +30,7 @@ public class MaximalValue {
         max = Math.max(Math.max(l,r), root.val);
         return max;
     }
-    public int solve(Tree root) {
+    public int solve(Tree1 root) {
         if(root == null){
             return 0;
         }
@@ -38,9 +38,9 @@ public class MaximalValue {
         return count;
     }
     public static void main(String[] args) {
-        Tree root = new Tree(1);
-        root.right = new Tree(2);
-        root.right.left = new Tree(1);
+        Tree1 root = new Tree1(1);
+        root.right = new Tree1(2);
+        root.right.left = new Tree1(1);
         
 
         MaximalValue obj = new MaximalValue();
