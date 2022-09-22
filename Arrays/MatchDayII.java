97e7961827a30d8ceea958af4d974366e46d0419
Kajal Jaiswal <anshikajais2001@gmail.com>
2022-09-22 08:22:04 +0530

    Match Day II - GFG

---
+++ Arrays/MatchDayII.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class MatchDayII{
+    static ArrayList<Integer> max_of_subarrays(int arr[], int n, int k) {
+        // Your code here
+        Deque<Integer> q = new LinkedList<>();
+        ArrayList<Integer> ans=new ArrayList<Integer>();
+
+        for(int i=0;i<n;++i){
+            while(q.size()>0 && i-q.getFirst()>=k){
+                q.removeFirst();
+            }
+            while(q.size()>0 && arr[q.getLast()] <= arr[i]){
+                q.removeLast();
+            }
+            q.addLast(i);
+            if(i>=k-1){
+                ans.add(arr[q.getFirst()]);
+            }
+        }
+
+        return ans;
+    }
+}
