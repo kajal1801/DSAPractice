bb891cffe9b4b605cb165c7fde497c608eba720b
Kajal Jaiswal <anshikajais2001@gmail.com>
2022-09-22 17:25:19 +0530

    Zig Zag Traversal - BinarySearch

--- Trees/ZigZagTraversal.java
+++ Trees/ZigZagTraversal.java
@@ -4,14 +4,26 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
+class TreeNode14 {
+    int val;
+    TreeNode14 left;
+    TreeNode14 right;
+    TreeNode14() {}
+    TreeNode14(int val) { this.val = val; }
+    TreeNode14(int val, TreeNode14 left, TreeNode14 right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
 public class ZigZagTraversal {
-    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode14 root) {
         if(root == null){
             return new ArrayList<List<Integer>>();
         }
         List<List<Integer>> list = new ArrayList<List<Integer>>();
         
-        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        Queue<TreeNode14> q = new LinkedList<TreeNode14>();
         q.add(root);
         int i = 0;
         while(!q.isEmpty())
@@ -19,7 +31,7 @@ public class ZigZagTraversal {
             List<Integer> curr = new ArrayList<Integer>();
             int size = q.size();
             for(int j = 0; j < size;j++){
-                TreeNode temp = q.poll();
+                TreeNode14 temp = q.poll();
                 if(temp.left != null){
                     q.add(temp.left);
                 }
