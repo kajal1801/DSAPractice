5e94938ef8fd59091a814139df4aed28ebc6ba53
Kajal Jaiswal <anshikajais2001@gmail.com>
2022-09-26 16:11:28 +0530

    Frog Jump - CodeStudio

--- DynamicProgramming/FrogJump.java
+++ DynamicProgramming/FrogJump.java
@@ -45,3 +45,24 @@ public class FrogJump {
         return dp[n-1];
     }
  */
+
+ // Space OPtimization
+ /*
+
+ public static int frogJump(int n, int heights[]) {
+        int curr = 0;
+        int prev = 0;
+        int prev2 = 0;
+        for(int i = 1;i < n;i++){
+            int left = prev + Math.abs(heights[i] - heights[i-1]);
+            int right = Integer.MAX_VALUE;
+            if(i > 1){
+                right = prev2 + Math.abs(heights[i] - heights[i-2]);
+            }
+            curr = Math.min(left, right);
+            prev2 = prev;
+            prev = curr;
+        }
+        return prev;
+    }
+  */
