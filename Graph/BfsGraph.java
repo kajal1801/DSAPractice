45f28c26873af350a35cbbf864d729b3682bbe15
Kajal Jaiswal <anshikajais2001@gmail.com>
2022-10-06 23:30:50 +0530

    BFS of Graph - GFG

---
+++ Graph/BfsGraph.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class BfsGraph {
+    public ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+        ArrayList<Integer> bfs = new ArrayList<>();
+        boolean vis[] = new boolean[V];
+        Queue<Integer> q = new LinkedList<>();
+        
+        q.add(0);
+        vis[0] = true;
+        
+        while(!q.isEmpty()){
+            Integer node = q.poll();
+            bfs.add(node);
+            
+            for(Integer it : adj.get(node)){
+                if(!vis[it]){
+                    vis[it] = true;
+                    q.add(it);
+                }
+            }
+        }
+        return bfs;
+    }
+}
