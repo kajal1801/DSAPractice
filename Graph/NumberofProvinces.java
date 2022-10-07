22f50ef0be6bc1428ba9bc17f944a94c5851d10d
Kajal Jaiswal <anshikajais2001@gmail.com>
2022-10-08 01:18:07 +0530

    Number of Provinces - Leetcode

--- Graph/NumberofProvinces.java
+++ Graph/NumberofProvinces.java
@@ -1,4 +1,6 @@
 public class NumberofProvinces {
+
+    // DFS
     public void dfs(int node, int[][] isConnected, boolean vis[]){
         vis[node] = true;
         for(int j = 0; j < isConnected.length;j++){
@@ -21,3 +23,36 @@ public class NumberofProvinces {
         return count;
     }
 }
+
+
+// BFS Code
+
+/*
+    public void bfs(int node, int[][] isConnected, boolean vis[]){
+        vis[node] = true;
+        Queue<Integer> q = new LinkedList<>();
+        q.add(node);
+        
+        while(!q.isEmpty()){
+            int u = q.poll();
+            for(int j = 0; j < isConnected.length;j++){
+                if(!vis[j] && isConnected[u][j] == 1){
+                    vis[j] = true;
+                    q.add(j);
+                }
+            }
+        }
+    }
+    public int findCircleNum(int[][] isConnected) {
+        
+        boolean vis[] = new boolean[isConnected.length];
+        int count = 0;
+        for(int i = 0;i < isConnected.length;i++){
+            if(vis[i] == false){
+                count++;
+                bfs(i, isConnected, vis);
+            }
+        }
+        return count;
+    }
+ */
