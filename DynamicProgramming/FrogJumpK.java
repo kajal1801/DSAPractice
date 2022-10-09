dd4942115ddf9d48e704b26e1fcb1fc2dbf0fca8
Kajal Jaiswal <anshikajais2001@gmail.com>
2022-10-09 20:51:17 +0530

    Frog K Jump

--- DynamicProgramming/FrogJumpK.java
+++ DynamicProgramming/FrogJumpK.java
@@ -1,24 +1,31 @@
+import java.util.Arrays;
+
 public class FrogJumpK{
-    public int jumpKtimes(int ind, int[] height, int k){
+    public int jumpKtimes(int ind, int[] height, int[] dp, int k){
         if(ind == 0){
             return 0;
+        }
+        if(dp[ind] != -1){
+            return dp[ind];
         }
         int minStep = Integer.MAX_VALUE;
         int step = 0;
         for(int i = 1;i <= k;i++){
             if(ind-i >= 0){
-                step = jumpKtimes(ind-i, height, k) + Math.abs(height[ind] - height[ind-i]);
+                step = jumpKtimes(ind-i, height, dp, k) + Math.abs(height[ind] - height[ind-i]);
                 minStep = Math.min(minStep, step);
             }
             else{
                 break;
             }
         }
-        return minStep;
+        return dp[ind] = minStep;
     }
 
     public int frogJump(int[] height, int k){
-        return jumpKtimes(height.length-1, height, k);
+        int dp[] = new int[height.length];
+        Arrays.fill(dp, -1);
+        return jumpKtimes(height.length-1, height, dp, k);
     }
 }
 
