b7ad3b4178f38bc547f38b3a5e59b5d7a13ad670
kajal1801 <anshikajais2001@gmail.com>
2022-12-09 18:36:28 +0530

    Level Order II

--- Trees/LevelOrderII.java
+++ Trees/LevelOrderII.java
@@ -3,19 +3,31 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
+class TreeNode12 {
+    int val;
+    TreeNode12 left;
+    TreeNode12 right;
+    TreeNode12() {}
+    TreeNode12(int val) { this.val = val; }
+    TreeNode12(int val, TreeNode12 left, TreeNode12 right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    } 
+}
 public class LevelOrderII{
-    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+    public List<List<Integer>> levelOrderBottom(TreeNode12 root) {
         ArrayList<List<Integer>> list = new ArrayList<List<Integer>>();
         if(root == null){
             return list;
         }
-        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        Queue<TreeNode12> q = new LinkedList<TreeNode12>();
         q.add(root);
         while(!q.isEmpty()){
             ArrayList<Integer> curr = new ArrayList<Integer>();
             int size = q.size();
             for(int i = 0;i < size;i++){
-                TreeNode temp = q.poll();
+                TreeNode12 temp = q.poll();
                 if(temp != null){
                     curr.add(temp.val);
                 }
